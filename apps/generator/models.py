610ede4b6fc7e4856260699326fdbf1ad0849efc
daniel <tishakovdanyilgmail.com>
2023-02-16 21:20:07 +0000

    init new branch, add all files

---
+++ apps/generator/models.py
@@ -0,0 +1,19 @@
+# from django.db import models
+#
+#
+# class Animal(models.Model):
+#     email = models.CharField(max_length=100)
+#      = models.PositiveSmallIntegerField()
+#
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     modified_at = models.DateTimeField(auto_now=True)
+#
+#     is_auto_generated = models.BooleanField(default=False)
+#
+#     def __str__(self) -> str:
+#         return f"{self.name} - {self.age}"
+#
+#     __repr__ = __str__
+# from django.db import models
+#
+# # Create your models here.
---
+++ apps/generator/views.py
@@ -0,0 +1,21 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render
+from faker import Faker
+import secrets
+
+fake = Faker('en_US')
+
+
+def index(request: WSGIRequest):
+    login = fake.first_name().lower()
+    email = f"{login}{secrets.token_hex(1)}@gmail.com"
+    password = fake.password(8)
+    return render(
+        request=request,
+        template_name='index.html',
+        context={
+            'email': email,
+            'login': login,
+            'password': password,
+        }
+    )
